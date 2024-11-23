f63ae7f770324996e8bb5d8c47612face73d1431
Hao-IT01 <hao.it.dw@gmail.com>
2024-11-23 23:22:42 +0700

    Update function.py

--- function.py
+++ function.py
@@ -1,9 +1,16 @@
 import pandas as pd
 
-list_search = []
-c = ""
 
 def Search(x):
-    global list_search, c
-    c = input("Enter the keyword to search: ")
-    list_search = [item for item in x if c.lower() in item.lower()]
+    global list_search
+    global c
+    
+    list_search = []
+    c = input()
+
+    for i in x:
+        if c.lower() in i.lower():
+            list_search.append(i)
+
+    if len(list_search) == 0:
+        print("Name was not found!")
